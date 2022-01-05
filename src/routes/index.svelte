478e1d82c2c240a9c80c1d216a2a8d539750683d
masoniis <bott.m@comcast.net>
2022-01-04 19:32:18 -0700

    commit on 'main'

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1 class="text-gray-900 bg-yellow-400">Welcome to SvelteKit</h1>
+<h1 class="text-gray-900 bg-yellow-400">Welcome to SvelteKit i am mason lolol</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
