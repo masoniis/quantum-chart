f35e7b69b50396074718a0a923c93b37ae2c90c1
masoniisE <bott.m@comcast.net>
2022-01-07 07:38:14 -0700

    commit on 'master'

--- src/routes/contact-us.svelte
+++ src/routes/contact-us.svelte
@@ -71,7 +71,7 @@
 		<div class="pt-5">
 			<div class="flex justify-end">
 				<button
-					type="button"
+					type="reset"
 					class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 				>
 					Clear
