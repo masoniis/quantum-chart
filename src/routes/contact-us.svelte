05619035c6e67ebd406b8d860b57f369f3b35d99
dawso <kerndr@s.dcsdk12.org>
2022-01-07 08:14:22 -0700

    commit on 'master'

--- src/routes/contact-us.svelte
+++ src/routes/contact-us.svelte
@@ -50,6 +50,7 @@
 						name="email"
 						type="email"
 						autocomplete="email"
+						placeholder="you@example.com"
 						class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
 					/>
 				</div>
