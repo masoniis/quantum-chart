d968343b3707db5516ec6e9c1ddea94cf659a65a
masoniisE <bott.m@comcast.net>
2022-01-10 08:53:47 -0700

    commit on 'master'

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <title>BeyondGreen - Home</title>
+
 <main class="min-h-screen container mx-auto">
 	<h1 class="text-6xl text-center my-6">Beyond Green</h1>
 	<p class="text-center">We make your lawn our priority</p>
