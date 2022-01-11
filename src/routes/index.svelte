c46f6419d9bd478b5f5a5737bd295c3a8ec0f587
masoniis <bott.m@comcast.net>
2022-01-11 16:58:04 -0700

    commit on 'master'

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,13 @@
 <title>BeyondGreen - Home</title>
 
+<main class="min-h-screen bg-light-bg">
+	<div class="container mx-auto py-10 text-light-text text-center">
+		<h1 class="text-6xl">BeyondGreen</h1>
+		<p class="text-3xl py-5"> We doo stuff </p>
+
+	</div>
+
+
+</main>
+
 
