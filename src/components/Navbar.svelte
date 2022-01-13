d0a5d801c0cd8771fdb35ffffe71c1cae9d1ca5f
masoniisE <bott.m@comcast.net>
2022-01-13 08:34:10 -0700

    Dropdown advancements

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { page } from '$app/stores';
 	import Logo from '../components/Logo.svelte';
+	import DropIcon from '../components/Icons/DropIcon.svelte'
 
 	let background;
 	let text;
