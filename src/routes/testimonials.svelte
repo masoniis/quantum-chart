312e536245a7ceac99b79de11124ee4d71422f34
masoniis <bott.m@comcast.net>
2022-01-17 12:02:57 -0700

    Improved testimonials

--- src/routes/testimonials.svelte
+++ src/routes/testimonials.svelte
@@ -20,8 +20,7 @@
 					Learn why your neighbors love Beyondgreen
 				</h2>
 				<p class="my-6 text-lg text-gray-600 pt-10 mx-auto text-center">
-					Don't just take our word for it, read from our extensive list of case studies and customer
-					testimonials.
+					Don't just take our word for it, read from our extensive list of customer testimonials.
 				</p>
 				<button
 					use:scrollTo={'testimonial-section'}
@@ -32,112 +31,118 @@
 		</div>
 	</section>
 
+	<!-- Section 2 -->
 	<section
 		use:scrollRef={'testimonial-section'}
-		class="flex items-center justify-center py-16 bg-gray-100 min-w-screen pt-40"
+		class="flex items-center justify-center py-16 bg-gray-100 min-w-screen pt-28"
 	>
 		<div class="max-w-6xl px-12 mx-auto bg-gray-100 md:px-16 xl:px-10">
-			<div class="flex flex-col items-center">
-				<div class="w-full lg:w-1/2">
-					<blockquote
-						class="flex items-center justify-between w-full col-span-1 p-6 bg-white rounded-lg shadow"
-					>
-						<div class="flex flex-col pr-8">
-							<div class="relative pl-12">
-								<svg
-									class="absolute left-0 w-10 h-10 text-red-700 fill-current"
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 100 125"
-								>
-									<path
-										d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"
-									/>
-								</svg>
-								<p class="mt-2 text-sm text-gray-600 sm:text-base lg:text-sm xl:text-base">
-									Awesome product! And the customer service is exceptionally well.
-								</p>
-							</div>
+			<div class="grid grid-cols-1 grid-rows-3 lg:grid-cols-4 lg:grid-rows-2 items-center gap-10 w-full">
 
-							<h3
-								class="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base"
+				<!-- Card #1 -->
+				<blockquote
+					class="flex items-center justify-between w-full col-span-2 p-6 bg-white rounded-lg shadow min-h-full"
+				>
+					<div class="flex flex-col pr-8">
+						<div class="relative pl-12">
+							<svg
+								class="absolute left-0 w-10 h-10 text-red-700 fill-current"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 100 125"
 							>
-								Jane Cooper
-								<span class="mt-1 text-sm leading-5 text-gray-500 truncate">- Happy neighbor</span>
-							</h3>
+								<path
+									d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"
+								/>
+							</svg>
+							<p class="mt-2 text-sm text-gray-600 sm:text-base lg:text-sm xl:text-base">
+								Awesome product! And the customer service is exceptionally well.
+							</p>
 						</div>
-						<img
-							class="flex-shrink-0 w-20 h-20 bg-gray-300 rounded-full xl:w-24 xl:h-24"
-							src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
-							alt=""
-						/>
-					</blockquote>
-					<blockquote
-						class="flex items-center justify-between w-full col-span-1 p-6 mt-4 bg-white rounded-lg shadow"
-					>
-						<div class="flex flex-col pr-10">
-							<div class="relative pl-12">
-								<svg
-									class="absolute left-0 w-10 h-10 text-red-700 fill-current"
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 100 125"
-								>
-									<path
-										d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"
-										class=""
-									/>
-								</svg>
-								<p class="mt-2 text-sm text-gray-600 sm:text-base lg:text-sm xl:text-base">
-									I can't express enough, how amazing this service has been for my company.
-								</p>
-							</div>
-							<h3
-								class="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base"
-							>
-								John Doe
-								<span class="mt-1 text-sm leading-5 text-gray-500 truncate">- Happy neighbor</span>
-							</h3>
-							<p class="mt-1 text-sm leading-5 text-gray-500 truncate" />
-						</div>
-						<img
-							class="flex-shrink-0 w-24 h-24 bg-gray-300 rounded-full"
-							src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;aauto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
-							alt=""
-						/>
-					</blockquote>
-					<blockquote
-						class="flex items-center justify-between w-full col-span-1 p-6 mt-4 bg-white rounded-lg shadow"
-					>
-						<div class="flex flex-col pr-10">
-							<div class="relative pl-12">
-								<svg
-									class="absolute left-0 w-10 h-10 text-red-700 fill-current"
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 100 125"
-								>
-									<path
-										d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"
-									/>
-								</svg>
-								<p class="mt-2 text-sm text-gray-600 sm:text-base lg:text-sm xl:text-base">
-									I can't express enough, how amazing this service has been for my company.
-								</p>
-							</div>
 
-							<h3
-								class="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base"
+						<h3
+							class="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base"
+						>
+							Jane Cooper
+							<span class="mt-1 text-sm leading-5 text-gray-500 truncate">- Happy neighbor</span>
+						</h3>
+					</div>
+					<img
+						class="hidden sm:block flex-shrink-0 w-20 h-20 bg-gray-300 rounded-full xl:w-24 xl:h-24"
+						src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+						alt=""
+					/>
+				</blockquote>
+
+				<!-- Card #2-->
+				<blockquote
+					class="flex items-center justify-between w-full col-span-2 p-6 bg-white rounded-lg shadow min-h-full"
+				>
+					<div class="flex flex-col pr-10">
+						<div class="relative pl-12">
+							<svg
+								class="absolute left-0 w-10 h-10 text-red-700 fill-current"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 100 125"
 							>
-								John Smith
-								<span class="mt-1 text-sm leading-5 text-gray-500 truncate">- Happy neighbor</span>
-							</h3>
-							<p class="mt-1 text-sm leading-5 text-gray-500 truncate" />
+								<path
+									d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"
+									class=""
+								/>
+							</svg>
+							<p class="mt-2 text-sm text-gray-600 sm:text-base lg:text-sm xl:text-base">
+								I can't express enough, how amazing this service has been for my wellbeing and the
+								house too!
+							</p>
 						</div>
-						<img
-							class="flex-shrink-0 w-24 h-24 bg-gray-300 rounded-full"
-							src="https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixlib=rrb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;aauto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
-							alt=""
-						/>
-					</blockquote>
-				</div>
+						<h3
+							class="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base"
+						>
+							John Doe
+							<span class="mt-1 text-sm leading-5 text-gray-500 truncate">- Happy neighbor</span>
+						</h3>
+						<p class="mt-1 text-sm leading-5 text-gray-500 truncate" />
+					</div>
+					<img
+						class="hidden sm:block flex-shrink-0 w-24 h-24 bg-gray-300 rounded-full"
+						src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;aauto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+						alt=""
+					/>
+				</blockquote>
+
+				<!-- Card #3 -->
+				<blockquote
+					class="flex items-center justify-between w-full col-span-2 p-6 bg-white rounded-lg shadow min-h-full"
+				>
+					<div class="flex flex-col pr-10">
+						<div class="relative pl-12">
+							<svg
+								class="absolute left-0 w-10 h-10 text-red-700 fill-current"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 100 125"
+							>
+								<path
+									d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"
+								/>
+							</svg>
+							<p class="mt-2 text-sm text-gray-600 sm:text-base lg:text-sm xl:text-base">
+								I can't express enough, how amazing this service has been for my company.
+							</p>
+						</div>
+
+						<h3
+							class="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base"
+						>
+							John Smith
+							<span class="mt-1 text-sm leading-5 text-gray-500 truncate">- Happy neighbor</span>
+						</h3>
+						<p class="mt-1 text-sm leading-5 text-gray-500 truncate" />
+					</div>
+					<img
+						class="hidden sm:block flex-shrink-0 w-24 h-24 bg-gray-300 rounded-full"
+						src="https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixlib=rrb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;aauto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+						alt=""
+					/>
+				</blockquote>
 			</div>
 		</div>
 	</section>
