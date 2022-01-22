10b792802b575f971e23e8f89d885b1b89c64138
masoniis <bott.m@comcast.net>
2022-01-22 09:47:35 -0700

    Section divider

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,7 +21,7 @@
 
 <main class="min-h-screen text-maintext overflow-hidden">
 	<!-- Section 1 -->
-	<section class="relative min-w-screen bg-topsection pt-24 sm:pt-40">
+	<section class="relative min-h-screen min-w-screen bg-topsection pt-24 sm:pt-40">
 		<div class="max-w-6xl px-12 mx-auto xl:px-10 pt-8 pb-32">
 			<grid class="grid grid-cols-3 grid-rows-4 items-start justify-center w-full">
 				<h1
@@ -105,22 +105,22 @@
 				/>
 			</svg>
 		</div>
-
-		<!-- Wave Divider -->
-		<div class="topsection-wave bg-mainbg fill-topsection">
-			<svg
-				data-name="Layer 1"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 1200 120"
-				preserveAspectRatio="none"
-			>
-				<path
-					d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-					class="shape-fill"
-				/>
-			</svg>
-		</div>
 	</section>
+
+	<!-- Wave Divider -->
+	<div class="topsection-wave bg-mainbg fill-topsection">
+		<svg
+			data-name="Layer 1"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 1200 120"
+			preserveAspectRatio="none"
+		>
+			<path
+				d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+				class="shape-fill"
+			/>
+		</svg>
+	</div>
 
 	<!-- Section 2 -->
 	<section
