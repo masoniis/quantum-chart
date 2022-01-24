2f57d63e5a600b89ca3e951097e1715b2734f2d3
masoniis <bott.m@comcast.net>
2022-01-23 20:49:25 -0700

    Squiggle test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -52,22 +52,23 @@
 					TEST
 				{/if} -->
 				{#if squiggle}
-					<div class="col-span-3 self-center flex justify-center justify-self-start pt-2 pb-6 z-20 relative">
-						<svg
-							viewBox="200 0 1200 400"
-							xmlns="http://www.w3.org/2000/svg"
-							style="vector-effect: non-scaling-stroke; -webkit-transform:translateZ(1px)"
-						>
-							<path
-								d="m-2.99994,3c463.50597,367 708.06434,182 752.36473,255c44.30039,73 -176.98353,39 5.41698,-54c182.40051,-93 543.35036,13 746.51432,37"
-								opacity="NaN"
-								stroke-width="13"
-								stroke="#000"
-								fill="transparent"
-							/>
-						</svg>
-					</div>
-				{/if}
+				<div class="col-span-3 row-span-1 flex justify-center -mt-[2rem] my-12">
+					<svg
+						class="relative"
+						viewBox="200 0 1200 400"
+						xmlns="http://www.w3.org/2000/svg"
+						style="vector-effect: non-scaling-stroke;"
+					>
+						<path
+							d="m-2.99994,3c463.50597,367 708.06434,182 752.36473,255c44.30039,73 -176.98353,39 5.41698,-54c182.40051,-93 543.35036,13 746.51432,37"
+							opacity="NaN"
+							stroke-width="13"
+							stroke="#000"
+							fill="transparent"
+						/>
+					</svg>
+				</div>
+{/if}
 				<h2
 					class="col-span-3 mx-auto text-center max-w-xl text-sm px-2
 					xs:py-2
