de452e2c626efe9aef80ca8392f80ad0e2cdadb1
masoniis <bott.m@comcast.net>
2022-01-29 15:03:30 -0700

    Index Added Content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -192,21 +192,21 @@
 				class="hidden md:grid grid-cols-4 grid-rows-5 origin-center feature-grid w-[40rem] mr-12 mt-20 space-x-4 space-y-4"
 			>
 				<button
-					use:scrollTo={'easy-section'}
+					use:scrollTo={'low-cost'}
 					class="row-span-2 bg-gray-200 border mt-4 ml-4 rounded-2xl hover:shadow-xl transition-all hover:scale-[1.05]"
 				>
 					<!-- Low Cost Image -->
 					<img class="rounded-2xl h-full" src="low-cost.jpg" alt="Low Cost" />
 				</button>
 				<button
-					use:scrollTo={'easy-section'}
+					use:scrollTo={'easy'}
 					class="row-span-3 col-span-2 bg-gray-200 border rounded-2xl hover:shadow-xl transition-all flex-col hover:scale-[1.05]"
 				>
 					<!-- Easy to Use Image -->
 					<img class="rounded-2xl h-full" src="easy.jpg" alt="Easy to Use" />
 				</button>
 				<button
-					use:scrollTo={'easy-section'}
+					use:scrollTo={'satisfaction-guarenteed'}
 					class="row-span-2 bg-gray-200 border rounded-2xl hover:shadow-xl transition-all flex-col hover:scale-[1.05]"
 				>
 					<!-- Satisfaction Guarenteed Image -->
@@ -217,21 +217,21 @@
 					/>
 				</button>
 				<button
-					use:scrollTo={'reputation-section'}
+					use:scrollTo={'reputable'}
 					class="row-span-3 bg-gray-300 border rounded-2xl hover:shadow-xl transition-all flex-col hover:scale-[1.1]"
 				>
 					<!-- Reputable Image -->
 					<img class="rounded-2xl h-full" src="reputable.jpg" alt="Reputable" />
 				</button>
 				<button
-					use:scrollTo={'professional-section'}
+					use:scrollTo={'responsive'}
 					class="row-span-3 bg-gray-300 border rounded-2xl hover:shadow-xl transition-all flex-col hover:scale-[1.1]"
 				>
 					<!-- Responsive Image -->
 					<img class="rounded-2xl h-full" src="responsive.jpg" alt="Responsive" />
 				</button>
 				<button
-					use:scrollTo={'responsive-section'}
+					use:scrollTo={'risk-free'}
 					class="col-span-2 row-span-2 bg-gray-300 border rounded-2xl hover:shadow-xl transition-all flex-col hover:scale-[1.1]"
 				>
 					<!-- Risk Free Image -->
@@ -242,38 +242,38 @@
 	</section>
 
 	<!-- Section 3 -->
-	<section
-		use:scrollRef={'easy-section'}
-		class="container mx-auto grid grid-cols-1 grid-rows-5 md:grid-cols-3 md:grid-rows-2 p-12"
-	>
+	<section use:scrollRef={'easy'} class="container mx-auto my-24 pt-12">
 		<h1 class="md:col-span-3 rows-span-2 text-center text-6xl font-bold py-12">
 			This is how it works
 		</h1>
-		<div class="text-center">
-			<p class="h-24 w-24 mx-auto">
-				<Logo />
-			</p>
-			<h2 class="text-5xl py-4">Subscribe</h2>
-			<p>with a simple 16 step process</p>
-		</div>
-		<div class="text-center">
-			<p class="h-24 w-24 mx-auto">
-				<Logo />
-			</p>
-			<h2 class="text-5xl py-4">Describe</h2>
-			<p>exactly how you want the job done, and we will guartntee your satisfaction</p>
-		</div>
-		<div class="text-center">
-			<p class="h-24 w-24 mx-auto">
-				<Logo />
-			</p>
-			<h2 class="text-5xl py-4">Vibe</h2>
-			<p>inside while all the work is done by us on the outside</p>
-		</div>
+
+		<grid class="grid grid-cols-1 grid-rows-5 md:grid-cols-3 md:grid-rows-2 p-12">
+			<div class="text-center">
+				<p class="h-24 w-24 mx-auto">
+					<Logo />
+				</p>
+				<h2 class="text-5xl py-4">Subscribe</h2>
+				<p>with a simple 16 step process</p>
+			</div>
+			<div class="text-center">
+				<p class="h-24 w-24 mx-auto">
+					<Logo />
+				</p>
+				<h2 class="text-5xl py-4">Describe</h2>
+				<p>exactly how you want the job done, and we will guartntee your satisfaction</p>
+			</div>
+			<div class="text-center">
+				<p class="h-24 w-24 mx-auto">
+					<Logo />
+				</p>
+				<h2 class="text-5xl py-4">Vibe</h2>
+				<p>inside while all the work is done by us on the outside</p>
+			</div>
+		</grid>
 	</section>
 
 	<!-- Section 4 -->
-	<section use:scrollRef={'reputation-section'} class="m-72">
+	<section use:scrollRef={'reputable'} class="m-72">
 		<p class="text-center">Trusted by people and stuff</p>
 		<div class="bg-gray-50 pt-12 sm:pt-16">
 			<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -321,18 +321,67 @@
 		</div>
 	</section>
 
+	<section use:scrollRef={'low-cost'} class="container mx-auto">
+		<h1 class="text-center text-4xl pb-8">Always at your side</h1>
+
+		<grid class="grid grid-cols-4 grid-rows-1 gap-7">
+			<div
+				class="flex flex-col self-center md:self-start max-w-md shadow-lg bg-stone-50"
+				in:fly={{ duration: 1400, x: -700 }}
+			>
+				<h1 class="font-bold text-lg px-10 py-10 ">Cool Stuff</h1>
+				<div class="px-10 pb-10">Like this</div>
+			</div>
+
+			<div
+				class="flex flex-col self-center md:self-start max-w-md shadow-lg bg-stone-50"
+				in:fly={{ duration: 1400, x: -700 }}
+			>
+				<h1 class="font-bold text-lg px-10 py-10 ">export as interactive md</h1>
+				<div class="px-10 pb-10">Like this</div>
+			</div>
+
+			<div
+				class="flex flex-col self-center md:self-start max-w-md shadow-lg bg-stone-50"
+				in:fly={{ duration: 1400, x: -700 }}
+			>
+				<h1 class="font-bold text-lg px-10 py-10 ">Cool equations</h1>
+				<div class="px-10 pb-10">Like this</div>
+			</div>
+
+			<div
+				class="flex flex-col self-center md:self-start max-w-md shadow-lg bg-stone-50"
+				in:fly={{ duration: 1400, x: -700 }}
+			>
+				<h1 class="font-bold text-lg px-10 py-10 ">Ar and VR</h1>
+				<div class="px-10 pb-10">Like this</div>
+			</div>
+		</grid>
+
+		<h2 class="text-center text-2xl pt-8">Prices starting at $2.95/month, cancel anytime</h2>
+
+		<div class="rounded-md text-center pt-8">
+			<a
+				href="/pricing"
+				class="px-5 py-3 text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+			>
+				Get started
+			</a>
+		</div>
+	</section>
+
 	<!-- Section 4 -->
-	<section use:scrollRef={'professional-section'} class="m-72">
+	<section use:scrollRef={'satisfaction-guarenteed'} class="m-72">
 		<p class="text-center">PROFESSION SECITON</p>
 	</section>
 
 	<!-- Section 4 -->
-	<section use:scrollRef={'responsive-section'} class="m-72">
+	<section use:scrollRef={'responsive'} class="m-72">
 		<p class="text-center">RESPONSVIE SECITON</p>
 	</section>
 
 	<!-- Section 5 -->
-	<section use:scrollRef={'responsive-section'} class="m-72">
+	<section use:scrollRef={'risk-free'} class="m-72">
 		<h1 class="text-center text-4xl font-bold">100% risk-free</h1>
 
 		<p>
@@ -354,7 +403,7 @@
 			<div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
 				<div class="inline-flex rounded-md shadow">
 					<a
-						href="#"
+						href="/pricing"
 						class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
 					>
 						Get started
@@ -367,7 +416,6 @@
 
 <style>
 	.hero__flex {
-		height: 60vh;
 		display: -webkit-box;
 		display: -ms-flexbox;
 		display: flex;
--- src/stores.js
+++ src/stores.js
@@ -3,4 +3,4 @@ import { writable } from 'svelte/store'
 export const yStore = writable()
 export const menu = writable(false)
 export const contactModal = writable(false)
-export const companyName = writable("Charty Party")
+export const companyName = writable("Quantum Chart")
