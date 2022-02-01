437b4b45f87161149da76c4b2ca14279f93a33a0
masoniisE <bott.m@comcast.net>
2022-02-01 10:25:31 -0700

    Testimonial Section of Index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -166,8 +166,8 @@
 	</div>
 
 	<!-- Section 2 (Navigation Section) -->
-	<section class="container mx-auto my-24 pt-12">
-		<flex class="hero__flex perspective xl:justify-center mx-8 border-t-2 pt-16 border-black">
+	<section class="container mx-auto my-24">
+		<flex class="hero__flex perspective xl:justify-center mx-8">
 			<div class="max-w-xl z-[3] feature-grid-text pb-36">
 				<h2
 					class="text-6xl font-extrabold tracking-tight py-8"
@@ -289,7 +289,79 @@
 
 		<!-- Hobbyists -->
 		<div use:scrollRef={'hobbyists'} class="container mx-auto py-12 flex flex-row">
-			<div class="w-2/3">
+			<div class="w-2/3 flex flex-col gap-6">
+				<blockquote class="bg-mainbg shadow-lg rounded-lg flex flex-row justify-between w-3/4 p-2">
+					<div class="flex flex-col mx-auto">
+						<div class="relative">
+							<svg
+								class="2xs:absolute mx-auto 2xs:mx-0 left-0 w-10 h-10 text-red-700 fill-current"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 100 125"
+							>
+								<path
+									d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"
+								/>
+							</svg>
+							<p
+								class="mt-2 text-sm text-gray-600 
+							 	text-center	 
+								2xs:pl-12 2xs:text-left
+								sm:text-base 
+							   	lg:text-sm 
+								xl:text-base"
+							>
+								I love to play around with {$companyName} in my free time, it's just so satisfying.
+							</p>
+						</div>
+
+						<h3
+							class="items-center text-center 2xs:text-left 2xs:pl-12 mt-3 text-sm font-medium leading-5 text-maintext sm:text-base lg:text-base"
+						>
+							Jane Cooper <span class="hidden xs:inline text-gray-500"> - Happy Neighbor</span>
+						</h3>
+					</div>
+					<img
+						class="hidden m-2 sm:block sm:ml-4 w-24 h-24 bg-gray-300 rounded-full"
+						src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+						alt=""
+					/>
+				</blockquote>
+				<blockquote class="bg-mainbg shadow-lg rounded-lg flex flex-row justify-between w-3/4 p-2">
+					<div class="flex flex-col mx-auto">
+						<div class="relative">
+							<svg
+								class="2xs:absolute mx-auto 2xs:mx-0 left-0 w-10 h-10 text-red-700 fill-current"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 100 125"
+							>
+								<path
+									d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"
+								/>
+							</svg>
+							<p
+								class="mt-2 text-sm text-gray-600 
+							 	text-center	 
+								2xs:pl-12 2xs:text-left
+								sm:text-base 
+							   	lg:text-sm 
+								xl:text-base"
+							>
+								I love to play around with {$companyName} in my free time, it's just so satisfying.
+							</p>
+						</div>
+
+						<h3
+							class="items-center text-center 2xs:text-left 2xs:pl-12 mt-3 text-sm font-medium leading-5 text-maintext sm:text-base lg:text-base"
+						>
+							Jane Cooper <span class="hidden xs:inline text-gray-500"> - Happy Neighbor</span>
+						</h3>
+					</div>
+					<img
+						class="hidden m-2 sm:block sm:ml-4 w-24 h-24 bg-gray-300 rounded-full"
+						src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+						alt=""
+					/>
+				</blockquote>
 				<blockquote class="bg-mainbg shadow-lg rounded-lg flex flex-row justify-between w-3/4 p-2">
 					<div class="flex flex-col mx-auto">
 						<div class="relative">
