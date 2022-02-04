917ebd496e11e337acaeedcaa69ff691ae95c354
masoniisE <bott.m@comcast.net>
2022-02-04 09:16:56 -0700

    Testing sticky element

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -6,7 +6,7 @@
 
 <title>{$companyName} - About</title>
 
-<main class="min-h-screen text-maintext overflow-hidden">
+<main class="min-h-screen text-maintext">
 	<!-- Top Section -->
 	<section class="min-w-screen min-h-screen pt-40">
 		<div class="flex flex-row container max-w-6xl px-12 mx-auto xl:px-10 space-x-6">
@@ -24,15 +24,13 @@
 	</section>
 
 	<!-- Our Mission -->
-	<section class="min-w-screen min-h-screen pt-40">
-		<div class="flex flex-row container max-w-6xl px-12 mx-auto xl:px-10 space-x-6">
-			<h2 class="text-6xl text-left font-bold tracking-tight text-gray-900 sm:leading-none">
+    <section class="min-w-screen min-h-screen pt-40">
+		<div class="flex flex-row container bg-gray-200 max-w-6xl px-12 mx-auto xl:px-10 space-x-6 min-h-screen">
+			<h2 class="text-6xl h-fit sticky overflow-hidden min-w-fit top-[100px] text-left font-bold tracking-tight text-gray-900 sm:leading-none">
 				Our mission
 			</h2>
-		</div>
-		<div class="flex flex-row text-left">
 			<p>
-				{$companyName} started with the question, what if? What if we changed the way charting software worked, making it easier and more accessible for everyone? What if we used unique AR and VR software to make chart designing more interactive and enjoyable? What if we could make it simple for everyone of all ages to make any chart they want? At {$companyName}, we set out to make all those dreams come true in a single software.
+			    {$companyName} started with the question, what if? What if we changed the way charting software worked, making it easier and more accessible for everyone? What if we used unique AR and VR software to make chart designing more interactive and enjoyable? What if we could make it simple for everyone of all ages to make any chart they want? At {$companyName}, we set out to make all those dreams come true in a single software.
 			</p>
 		</div>
 	</section>
---
+++ src/routes/testing/sticky.svelte
@@ -0,0 +1,12 @@
+<main>
+    <section class="min-w-screen min-h-screen pt-40">
+		<div class="flex flex-row container bg-gray-200 max-w-6xl px-12 mx-auto xl:px-10 space-x-6 min-h-screen">
+			<h2 class="text-6xl h-fit sticky overflow-hidden min-w-fit top-[100px] text-left font-bold tracking-tight text-gray-900 sm:leading-none">
+				Our mission
+			</h2>
+			<p>
+			     started with the question, what if? What if we changed the way charting software worked, making it easier and more accessible for everyone? What if we used unique AR and VR software to make chart designing more interactive and enjoyable? What if we could make it simple for everyone of all ages to make any chart they want? At , we set out to make all those dreams come true in a single software.
+			</p>
+		</div>
+	</section>
+</main>
