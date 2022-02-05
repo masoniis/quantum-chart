3e9d64c25d9472a1cc2187dbc13d3e10d1ea0fcb
dawso <kerndr@s.dcsdk12.org>
2022-02-04 21:03:50 -0700

    About Page 3

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -9,10 +9,12 @@
 	<!-- Top Section -->
 	<section class="min-w-screen min-h-screen pt-40">
 		<div class="flex flex-row container max-w-6xl px-12 mx-auto xl:px-10 space-x-6">
-			<h2 class="text-6xl text-left font-bold tracking-tight text-gray-900 sm:leading-none">
+			<h2 class="text-2xl xs:text-4xl sm:text-5xl md:text-6xl text-center font-bold tracking-tight text-gray-900 sm:leading-none">
+				<div class="mx-auto md:hidden h-[128px] w-[128px]">
+					<Logo />
+				</div>
 				Learn what {$companyName} is really about
-
-				<p class="text-xl text-left font-normal pt-6">
+				<p class="text-sm xs:text-lg text-left font-normal pt-6">
 					At {$companyName}, our top priority is to provide the best possible service to every
 					single person who uses our products. We are constantly working to enhance, expand, and
 					elevate our products to standards beyond those of any other company. We make charts
@@ -20,7 +22,7 @@
 					chart-creation industry.
 				</p>
 			</h2>
-			<p class="h-[512px] w-[512px]">
+			<p class="md:h-[512px] md:w-[512px]">
 				<Logo />
 			</p>
 		</div>
@@ -33,11 +35,11 @@
 	<section class="bg-gray-200 py-16 my-8">
 		<flex class="flex flex-col lg:flex-row justify-center mx-auto container gap-6">
 			<h2
-				class="mx-auto text-center text-6xl h-fit lg:sticky overflow-hidden min-w-fit top-[88px] font-bold tracking-tight text-gray-900 sm:leading-none"
+				class="mx-auto text-center text-5xl xs:text-6xl h-fit lg:sticky overflow-hidden min-w-fit top-[88px] font-bold tracking-tight text-gray-900 sm:leading-none"
 			>
 				Our Mission
 			</h2>
-			<p class="lg:w-1/2 text-lg">
+			<p class="lg:w-1/2 text-sm xs:text-lg text-center">
 				{$companyName} started with the question, what if? What if we changed the way charting software
 				worked, making it easier and more accessible for everyone? What if we used unique AR and VR software
 				to make chart designing more interactive and enjoyable? What if we could make it simple for everyone
@@ -57,11 +59,11 @@
 	<section class="bg-gray-200 py-16 my-8">
 		<flex class="flex flex-col lg:flex-row justify-center mx-auto container gap-6">
 			<h2
-				class="mx-auto text-center text-6xl h-fit lg:sticky overflow-hidden min-w-fit top-[88px] font-bold tracking-tight text-gray-900 sm:leading-none"
+				class="mx-auto text-center text-5xl xs:text-6xl h-fit lg:sticky overflow-visible min-w-fit top-[88px] font-bold tracking-tight text-gray-900 sm:leading-none"
 			>
 				Our Story
 			</h2>
-			<p class="lg:w-1/2 text-lg">
+			<p class="lg:w-1/2 text-lg text-center">
 				Our two founders, John and Paul, met as college roommates, both pursuing a career in
 				software development. They both ended up working on similar projects: creating a program
 				that could take data and turn it into a line graph. When they realized that they were both
@@ -74,20 +76,20 @@
 
 	<!-- Founder Bios -->
 	<section>
-		<div class="flex flex-col lg:flex-row gap-6">
+		<div class="flex flex-col lg:flex-row lg:gap-6">
 			<div class="flex-col text-center justify-center mx-auto xl:px-10 w-50">
-				<img src='John.jpg' alt="John" width="500" height="1000" class='text-center'/>
+				<img src='John.jpg' alt="John" width="500" height="1000" class='mx-auto'/>
 				<h3 class="text-2xl my-2 mb-1 text-center font-bold text-gray-700">John</h3>
-				<p>
+				<p class='text-center'>
 					"I never saw myself where I am today--a young entrepreneur, partnering with a college
 					roommate to spend five years creating a charting app. But I don't regret a minute of it. {$companyName}
 					feels like a part of me now and I love seeing people's positive opinions of it."
 				</p>
 			</div>
 			<div class="flex-col justify-center mx-auto xl:px-10 w-50">
-				<img src='Paul.png' alt="Paul" width="500" height="1000" class='justify-center'/>
+				<img src='Paul.png' alt="Paul" width="500" height="1000" class='mx-auto'/>
 				<h3 class="text-2xl mt-2 mb-1 text-center font-bold text-gray-700">Paul</h3>
-				<p>
+				<p class='text-center'>
 					"I've dreamt of creating my own app since middle school. Now, with {$companyName}, that
 					dream has become a reality for me. I hope that {$companyName} can make life easier for people
 					who want to create presentations or anything else that needs charts."
