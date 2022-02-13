71232a05bb7c1493f61a081450cccb95681f16a5
masoniis <bott.m@comcast.net>
2022-02-12 18:33:29 -0700

    Index navigation modular component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,65 +1,19 @@
 <script>
-	import { companyName, statusBar } from '$stores';
+	import { companyName } from '$stores';
 	import { scrollTo, scrollRef, setGlobalOptions } from 'svelte-scrolling';
-	import { onMount } from 'svelte';
-	import { slide, fade, fly } from 'svelte/transition';
+	import { fly } from 'svelte/transition';
 	import { cubicInOut } from 'svelte/easing';
 	import Logo from '$icons/Logo.svelte';
-	import Modal from '$components/Modal.svelte';
 	import Blockquote from '$components/Blockquote.svelte';
 	import SectionHeader from '$pages/index/SectionHeader.svelte';
 	import IndexDividers from '$pages/index/IndexDividers.svelte';
 	import SectionIntro from '$pages/index/SectionIntro.svelte';
+	import SectionNavigation from '$pages/index/SectionNavigation.svelte';
 
 	setGlobalOptions({
 		duration: 400,
 		offset: -80,
 		easing: cubicInOut
-	});
-
-	const words = [
-		'simple',
-		'amazing',
-		'efficient',
-		'awesome',
-		'sleek',
-		'seamless',
-		'quick',
-		'beautiful',
-		'immersive'
-	];
-
-	const colors = [
-		'#61C9A8', //Ocean Green 			(green)
-		'#BBC8CA', //Silver Sand 			(blue)
-		'#B592A0', //Lilac Luster 			(lilac)
-		'#677DB7', //Glaucous 				(blue)
-		'#FB3640', //Red Salsa 				(red)
-		'#ED9B40', //Carrot Orange 			(orange)
-		'#EB6534', //Flame					(orange)
-		'#DBAFC1', //Cameo Pink				(pink)
-		'#92B4A7' //Cambridge Blue			(cyan-like)
-	];
-
-	const randomColor = () => {
-		return Math.round(Math.random() * (colors.length - 1));
-	};
-
-	let index = 0;
-	let randColor = randomColor();
-
-	onMount(() => {
-		const interval = setInterval(() => {
-			index = index + 1;
-			randColor = randomColor();
-			if (index > words.length - 1) {
-				index = 0;
-			}
-		}, 2000);
-
-		return () => {
-			clearInterval(interval);
-		};
 	});
 </script>
 
@@ -71,119 +25,7 @@
 
 	<IndexDividers type="intro.bot" />
 
-	<!-- Section 2 (Navigation Section) -->
-	<section class="container mx-auto my-24">
-		<flex
-			class="flex justify-center md:justify-start md:items-start perspective xl:justify-center max-h-64 md:max-h-fit"
-		>
-			<div class="max-w-xl z-[3] feature-grid-text pb-48">
-				<h2
-					class="font-extrabold tracking-tight py-8 
-						text-3xl
-						xs:text-4xl text-center
-						sm:text-5xl sm:px-8
-						md:text-6xl md:text-left md:min-w-[38rem] md:px-0"
-					style="max-width: 38rem;"
-				>
-					With {$companyName}, data has never been so
-					{#key index}
-						<p
-							in:fade={{ duration: 400 }}
-							class="md:inline before:content-['['] before:text-maintext after:content-['].'] after:text-maintext"
-							style="color: {colors[randColor]}"
-						>
-							{words[index]}
-						</p>
-					{/key}
-				</h2>
-				<p
-					class="text-md mx-auto text-center max-w-sm 
-						md:mx-0 md:text-left md:text-xl md:max-w-3xl"
-				>
-					Unrivaled service, revolutionary technology, and low prices, {$companyName} is a service like
-					no other.
-				</p>
-			</div>
-
-			<!-- 3d Panel -->
-			<grid
-				class="hidden md:grid grid-cols-4 grid-rows-5 origin-center feature-grid w-[40rem] mr-12 mt-20 space-x-4 space-y-4"
-			>
-				<button
-					use:scrollTo={'low-cost'}
-					class="row-span-2 bg-gray-200 border mt-4 ml-4 rounded-2xl hover:shadow-xl transition-all hover:scale-[1.05]"
-				>
-					<!-- Low Cost Image -->
-					<img
-						class="rounded-2xl h-[120px]"
-						style="background-color: #666766"
-						src="panel/low-cost.jpg"
-						alt="Low Cost"
-					/>
-				</button>
-				<button
-					use:scrollTo={'easy'}
-					class="row-span-3 col-span-2 bg-gray-200 border rounded-2xl hover:shadow-xl transition-all flex-col hover:scale-[1.05]"
-				>
-					<!-- Easy to Use Image -->
-					<img
-						class="rounded-2xl h-48"
-						style="background-color: #666766"
-						src="panel/easy-to-use.jpg"
-						alt="Easy to Use"
-					/>
-				</button>
-				<button
-					use:scrollTo={'risk-free'}
-					class="row-span-2 bg-gray-200 border rounded-2xl hover:shadow-xl transition-all flex-col hover:scale-[1.05]"
-				>
-					<!-- Satisfaction Guarenteed Image -->
-					<img
-						class="rounded-2xl h-[122px]"
-						style="background-color: #666766"
-						src="panel/satisfaction-guarenteed.jpg"
-						alt="Satisfaction Guarenteed"
-					/>
-				</button>
-				<button
-					use:scrollTo={'reputable'}
-					class="row-span-3 bg-gray-300 border rounded-2xl hover:shadow-xl transition-all flex-col hover:scale-[1.05]"
-				>
-					<!-- Reputable Image -->
-					<img
-						class="rounded-2xl h-[190px]"
-						style="background-color: #666766"
-						src="panel/reputable.jpg"
-						alt="Reputable"
-					/>
-				</button>
-				<button
-					use:scrollTo={'responsive'}
-					class="row-span-3 bg-gray-300 border rounded-2xl hover:shadow-xl transition-all flex-col hover:scale-[1.05]"
-				>
-					<!-- Responsive Image -->
-					<img
-						class="rounded-2xl h-[191px]"
-						style="background-color: #666766"
-						src="panel/responsive.jpg"
-						alt="Responsive"
-					/>
-				</button>
-				<button
-					use:scrollTo={'risk-free'}
-					class="col-span-2 row-span-2 bg-gray-300 border rounded-2xl hover:shadow-xl transition-all flex-col hover:scale-[1.05]"
-				>
-					<!-- Risk Free Image -->
-					<img
-						class="rounded-2xl h-[122px]"
-						style="background-color: #666766"
-						src="panel/risk-free.jpg"
-						alt="Risk Free"
-					/></button
-				>
-			</grid>
-		</flex>
-	</section>
+	<SectionNavigation />
 
 	<!-- EASY - How it works (Section 3) -->
 	<section use:scrollRef={'easy'} class="container mx-auto py-16 pb-32">
