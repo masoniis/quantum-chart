1a5c45f40b27d932217d494e832c4eb89104c3db
masoniis <bott.m@comcast.net>
2022-02-13 17:32:42 -0700

    Status bar test

--- src/lib/pages/index/SectionHeader.svelte
+++ src/lib/pages/index/SectionHeader.svelte
@@ -1,6 +1,21 @@
 <script>
     import {companyName, statusBar } from '$stores'
+
+	let y;
+	let show = true;
+
+	$: {
+	if (y > 70) {
+		show = true;
+		console.log("Show set true")
+	} else {
+		show = false;
+		console.log("Show set to false")
+	}
+}
 </script>
+
+<svelte:window bind:scrollY={y} />
 
 <svelte:head>
 	<title>Home - {$companyName}</title>
@@ -9,5 +24,5 @@
 		content="{$companyName} home page, make beautiful charts in the blink of an eye."
 	/>
 
-	<meta name="theme-color" content="#014473">
+	<meta name="theme-color" content="bg-lightbg">
 </svelte:head>
