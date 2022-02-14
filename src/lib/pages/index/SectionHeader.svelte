dc43aa6be96f584f1072b3d868883d012bf90c6f
masoniis <bott.m@comcast.net>
2022-02-13 18:00:00 -0700

    Status bar transition test

--- src/lib/pages/index/SectionHeader.svelte
+++ src/lib/pages/index/SectionHeader.svelte
@@ -1,65 +1,47 @@
 <script>
-    import {companyName, statusBar } from '$stores';
-	import { fade } from 'svelte/transition';
+	import { companyName, statusBar } from '$stores';
 	import { tweened } from 'svelte/motion';
+	import { interpolateLab } from 'd3-interpolate';
 
-    import { interpolateLab } from 'd3-interpolate';
+	const colors = ['rgb(224, 207, 155)', 'rgb(255, 255, 255)'];
 
-
-    const colors = [
-        'rgb(224, 207, 155)',
-        'rgb(255, 255, 255)',
-    ];
-
-    const color = tweened(colors[0], {
-        duration: 800,
-        interpolate: interpolateLab
-    });
-
-
-	const opactiy = tweened(0);
+	const color = tweened(colors[0], {
+		duration: 300,
+		interpolate: interpolateLab
+	});
 
 	let y;
 	let show = true;
 
 	$: {
-if (y < 60) {
-	show = true;
-	color.set('rgb(224, 207, 155)');
-	console.log("Show set true")
-} else {
-	show = false;
-	console.log("Show set to false")
-	color.set('rgb(255, 255, 255)');
-}
-}
+		if (y < 60) {
+			color.set('rgb(224, 207, 155)'); //Set color to tanish
+		} else {
+			color.set('rgb(255, 255, 255)'); //Set color to white
+		}
+	}
 </script>
 
 <svelte:window bind:scrollY={y} />
 
 <svelte:head>
-<title>Home - {$companyName}</title>
-<meta
-	name="description"
-	content="{$companyName} home page, make beautiful charts in the blink of an eye."
-/>
+	<title>Home - {$companyName}</title>
+	<meta
+		name="description"
+		content="{$companyName} home page, make beautiful charts in the blink of an eye."
+	/>
 
-{#if show}
-	<meta name="theme-color" content="{$color}">
-{/if}
+	<meta name="theme-color" content={$color} />
 </svelte:head>
 
 <br /><br /><br /><br /><br /><br />
 
 {#each colors as c}
-<button
-	style="background-color: {c}; color: white; border: none;"
-	on:click="{e => color.set(c)}"
-    >{c}</button>
+	<button style="background-color: {c}; color: white; border: none;" on:click={(e) => color.set(c)}
+		>{c}</button
+	>
 {/each}
 
-<h1>
-	Hey i am 
-</h1>
+<h1>Hey i am</h1>
 
 <h1 style="color: {$color}">{$color}</h1>
